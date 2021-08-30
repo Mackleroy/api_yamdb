1afe956105d53db5d1f8cbfa1b017439b9526054
Mackleroy <gatt2508@gmail.com>
2021-08-30 16:22:40 +0300

    Delete extra files

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -152,8 +152,8 @@ REST_FRAMEWORK = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'al.gattunen@gmail.com'
-EMAIL_HOST_PASSWORD = 'Trav8-9-0'
+EMAIL_HOST_USER = '************'
+EMAIL_HOST_PASSWORD = '*******'
 EMAIL_PORT = 587
 
 
